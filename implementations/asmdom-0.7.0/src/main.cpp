f94d8d6387044cbd6669cc443659f1fcc432db82
Rupert Smith <rupert@thesett.com>
2024-02-02 17:11:28 +0000

    Adding the view for todo entries.

--- implementations/asmdom-0.7.0/src/main.cpp
+++ implementations/asmdom-0.7.0/src/main.cpp
@@ -16,6 +16,15 @@ Model model = {
         .visibility = "All"
 };
 
+Entry oneEntry = {
+        .description = "An Entry",
+        .completed = true,
+        .editing = false,
+        .id = 1
+};
+
+
+
 VNode *current_view = nullptr;
 
 bool Decrease(emscripten::val) {
@@ -32,8 +41,6 @@ bool Increase(emscripten::val) {
     return true;
 }
 
-
-
 int main() {
     // Initialize asm-dom.
     asmdom::Config config = asmdom::Config();
@@ -44,6 +51,9 @@ int main() {
     emscripten::val root = document.call<emscripten::val>("getElementById", std::string("root"));
     current_view = asmdom::h(u8"div", std::string(u8"Initial view"));
     asmdom::patch(root, current_view);
+
+    // Build an example.
+    model.entries.push_back(oneEntry);
 
     // Update the virtual dom.
     VNode* new_node = view(model);
